518463f5d46df896b2bc06d46c14951a07485ed2
Mateusz Aszyk <mateusz.aszyk@bluesoft.com>
2020-08-05 14:53:01 +0200

    Change SQL query for PGSQL

--- lms/djangoapps/courseware/migrations/0011_csm_id_bigint.py
+++ lms/djangoapps/courseware/migrations/0011_csm_id_bigint.py
@@ -21,9 +21,16 @@ class CsmBigInt(AlterField):
         to_model = to_state.apps.get_model(app_label, self.model_name)
         if schema_editor.connection.alias == 'student_module_history':
             if settings.FEATURES["ENABLE_CSMH_EXTENDED"]:
-              schema_editor.execute("ALTER TABLE `coursewarehistoryextended_studentmodulehistoryextended` MODIFY `student_module_id` bigint UNSIGNED NOT NULL;")
+		        if schema_editor.connection.vendor == 'postgresql':
+                    schema_editor.execute("ALTER TABLE coursewarehistoryextended_studentmodulehistoryextended ALTER COLUMN student_module_id TYPE bigint UNSIGNED NOT NULL;")
+		        else:
+                    schema_editor.execute("ALTER TABLE coursewarehistoryextended_studentmodulehistoryextended MODIFY student_module_id bigint UNSIGNED NOT NULL;")
+
         elif self.allow_migrate_model(schema_editor.connection.alias, to_model):
-            schema_editor.execute("ALTER TABLE `courseware_studentmodule` MODIFY `id` bigint UNSIGNED AUTO_INCREMENT NOT NULL;")
+            if schema_editor.connection.vendor == 'postgresql':
+                schema_editor.execute("ALTER TABLE courseware_studentmodule ALTER COLUMN id TYPE bigint UNSIGNED AUTO_INCREMENT NOT NULL;")
+            else:
+                schema_editor.execute("ALTER TABLE courseware_studentmodule MODIFY id bigint UNSIGNED AUTO_INCREMENT NOT NULL;")
 
     def database_backwards(self, app_label, schema_editor, from_state, to_state):
         # Make backwards migration a no-op, app will still work if column is wider than expected
