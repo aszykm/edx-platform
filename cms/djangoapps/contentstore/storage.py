0b9cedd06364fc0b89b027b8c408da87aeb7ac02
Mateusz Aszyk <mateusz.aszyk@bluesoft.com>
2020-08-27 17:04:44 +0200

    Revert "Add azure storage to courses"
    This reverts commit ebe845eec549c8987d356cedbb2cc8e9d60d96f5.

--- cms/djangoapps/contentstore/storage.py
+++ cms/djangoapps/contentstore/storage.py
@@ -6,14 +6,7 @@ from __future__ import absolute_import
 from django.conf import settings
 from django.core.files.storage import get_storage_class
 from storages.backends.s3boto import S3BotoStorage
-from storages.backends.azure_storage import AzureStorage
 from storages.utils import setting
-
-
-class ImportExportAzureStorage(AzureStorage):
-    def __init__(self):
-        azure_container = setting("COURSE_IMPORT_EXPORT_CONTAINER", settings.AZURE_STORAGE_CONTAINER_NAME)
-        super(ImportExportAzureStorage, self).__init(container=azure_container)
 
 
 class ImportExportS3Storage(S3BotoStorage):  # pylint: disable=abstract-method
